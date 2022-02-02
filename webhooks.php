06f97d4f8185477779acf71f6447121bdc0f7094
Moe <wiranee.poungkaew@gmail.com>
2022-02-02 15:17:42 +0700

    test tag

--- webhooks.php
+++ webhooks.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
                 // Build message to reply back
                 $messages = [
                     'type' => 'text',
-                    'text' => '@jade test'
+                    'text' => json_encode($event)
                 ];
 
 
